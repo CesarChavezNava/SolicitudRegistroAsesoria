1f07f2201a33e9bdeea202025b925e6289c56f25
Cesar Nava Chavez <Cesar Nava Chavez>
2016-09-04 00:56:18 -0500

    Directiva perDate Actualizada

--- dev/app/directives/dateDirective.js
+++ dev/app/directives/dateDirective.js
@@ -2,9 +2,10 @@ app.directive('perDate', function(){
     return {
         restrict: 'E',
         scope: {
+            clase: '@clase',
             id: '@id',
             texto: '@texto'
         },
-        template: '<input id="{{ id }}" type="date" value=""><label for="{{ id }}" class="active">{{ texto }}</label>'
+        template: '<div class="{{ clase }}"><input id="{{ id }}" type="date" value=""><label for="{{ id }}" class="active">{{ texto }}</label></div>'
     }
 });
